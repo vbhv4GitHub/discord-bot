1bf02c4eebb1a79e79acbe2dfbd79032b4af786f
vbhv4GitHub <ravi9406126068@gmail.com>
2022-10-01 23:06:00 +0530

    feat(project): :tada: begin discord bot project

---
+++ index.ts
@@ -0,0 +1,20 @@
+import dotenv from 'dotenv';
+import { Client, GatewayIntentBits } from 'discord.js';
+
+dotenv.config();
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.on('ready', () => {
+	console.log(`Logged in as ${client.user?.tag}!`);
+});
+
+client.on('interactionCreate', async (interaction) => {
+	if (!interaction.isChatInputCommand()) return;
+
+	if (interaction.commandName === 'ping') {
+		await interaction.reply('Pong!');
+	}
+});
+
+client.login(process.env.DISCORD_TOKEN);
