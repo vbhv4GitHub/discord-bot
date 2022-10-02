3839a39cf3dd5d04f8c286e8fe5f2cb037bd74ba
vbhv4GitHub <ravi9406126068@gmail.com>
2022-10-02 16:29:31 +0530

    chore(script): :package: add postinstall script to package.json

--- index.ts
+++ index.ts
@@ -3,7 +3,17 @@ import { Client, GatewayIntentBits } from 'discord.js';
 
 dotenv.config();
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMembers,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.DirectMessages,
+		GatewayIntentBits.MessageContent,
+		GatewayIntentBits.DirectMessageTyping,
+		GatewayIntentBits.DirectMessageReactions,
+	],
+});
 
 client.on('ready', () => {
 	console.log(`${client.user?.tag} is now launched to the orbit!! 🚀`);
@@ -14,6 +24,10 @@ client.on('messageCreate', (message) => {
 	if (message.content === 'ping') {
 		message.channel.send('Pong!');
 	}
+});
+
+client.on('messageDelete', (message) => {
+	message.channel.send('Stop Deleting messages!!! 😠');
 });
 
 client.login(process.env.BOT_TOKEN);
